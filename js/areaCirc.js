3eaf87ebc23714db55a41cc05a94f03b99dab533
yuripregeljhoffmann <hoffmannyuri711@gmail.com>
2024-03-04 11:25:31 -0300

    aula 04/03/2024

---
+++ js/areaCirc.js
@@ -0,0 +1,8 @@
+var r = 5;
+var pi = 3.14;
+
+var area = pi * (r*r);
+
+console.log(" Area Circunferência: " + area);
+
+
---
+++ js/hello.js
@@ -0,0 +1,12 @@
+var num1 = 78;
+var num2 = 15;
+
+var result = num1 + num2;
+
+console.log(num1 + "+" + num2 + "=" + result);
+
+var nome = "Yuri";
+console.log("Meu nome é"+ nome);
+
+var moeda = 0.5;
+
