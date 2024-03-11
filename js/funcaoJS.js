bfd9b6f55698f60c813dfc4cb7416f34627b3791
yuripregeljhoffmann <hoffmannyuri711@gmail.com>
2024-03-11 11:33:10 -0300

    aula11032024

---
+++ js/funcaoJS.js
@@ -0,0 +1,17 @@
+let cor = "vermelho";  //azul e verde //variavel global
+
+function mostrarAula()
+{
+let aula = "Usabilidade"; //variavel global
+ console.log(aula);
+ console.log(cor);
+
+}
+ function mostrarCor()
+
+ {
+    console.log(cor);
+    console.log(aula);
+ }
+
+
